bb86297536fdaff743a3c394472f237655f30b12
sillyy <965768042@qq.com>
2019-08-08 23:10:09 +0800

    dev pagination

---
+++ packages/Pagination/src/Pagination.vue
@@ -0,0 +1,47 @@
+<template>
+    <el-pagination
+    background
+    @size-change='sizeCb'
+    @current-change='pageCb'
+    :current-page='currentPage'
+    :page-sizes='pageSizes'
+    :page-size='pageSize'
+    :layout='layout'
+    :total='total'
+    v-if='total>0' />
+</template>
+<script>
+export default {
+    name: 'Pagination',
+    props:{
+        layout: {
+            type: String,
+            default: 'total, prev, pager, next, sizes, jumper'
+        },
+        total: {
+            type: Number,
+            default: 0
+        },
+        pageSize: {
+            type: Number,
+            default: 20
+        },
+        currentPage: {
+            type: Number,
+            default: 1
+        },
+        sizeCb: {
+            type: Function,
+            default() {
+                return function() {}
+            }
+        },
+        pageCb: {
+            type: Function,
+            default() {
+                return function() {}
+            }
+        }
+    }
+}
+</script>
